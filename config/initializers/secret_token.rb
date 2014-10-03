b021069fb607e793ca93218c76e18a94bc70e853
Gregor <am@iworry.ru>
2014-10-03 16:06:11 +0400

    main page done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anya::Application.config.secret_key_base = 'f0e014a9689a489fc8593376e5880a5539168828105d297b9c115cf603ad566b0123031e136cdc98a6e12a340e52d0b76186ccee311449112ec7cdd956e96fdd'
